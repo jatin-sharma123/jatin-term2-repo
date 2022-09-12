6d5e6e92bbde06d28bcf3bbe713274ae70ea011e
jatin-sharma123 <111501547+jatin-sharma123@users.noreply.github.com>
2022-09-12 15:18:15 +0530

    Update jatin.java

--- jatin.java
+++ jatin.java
@@ -1,5 +1,5 @@
 class Main {
     public static void main(String[] args) {
-        System.out.println("Welcome to the world of GitHub");
+        System.out.println("Welcome to the world of GitLab");
     }
 }
